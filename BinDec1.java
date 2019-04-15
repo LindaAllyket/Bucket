1348df4ab69b8f1b01b1d6983164830955744888
LindaAllyket <Linda.Biasua@academytrainee.com>
2019-04-15 16:09:13 +0100

    Init Commit

--- BinDec1.java
+++ BinDec1.java
@@ -4,14 +4,9 @@ class BinDec{
 
 	public double Exercise1(String x){
 		System.out.println("This is the decimal notation of " + x + " : ");
-		System.out.println(" ");
 		String k;
-		int m;
-		m=x.length()-1;
-		int n;
-		int i;
+		int m=x.length()-1, n=0 , i;
 		double d=0;
-		n=0;
 		for(i=m; n<=m; i--){
 			
 			k=x.substring(i,i+1);
@@ -28,7 +23,7 @@ class BinDec{
 class BinDec1{
 	public static void main(String abc[]){
 		BinDec Z= new BinDec();
-		Z.Exercise1("1111000110001");
+		Z.Exercise1("11110110001");
 
 	}
 }
